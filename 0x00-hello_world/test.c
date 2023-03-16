26f5f0ae75ade0a9b01fabf9eadf351e7e5a3d3b
manucho254 <manuchoadero@gmail.com>
2023-03-16 12:26:07 +0000

    print string using puts

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Printing using puts command
+ *
+ * Always return 0 (success)
+ *
+ */
+
+
+int main(void){
+  /* char str[] = "\"Programming is like building a multilingual puzzle"; */
+
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+
+}
