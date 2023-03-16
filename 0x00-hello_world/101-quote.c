1b1e449022ce4adb5bc7b3417382948582bb3988
manucho254 <manuchoadero@gmail.com>
2023-03-16 14:09:40 +0000

    Print string without using printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Print data without using printf and puts
+ *
+ *
+ * Return: Always return 1 (error)
+ */
+
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
