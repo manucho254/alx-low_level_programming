517fd13b0a09c19e2c02c78bff3d0c2af6c4538b
manucho254 <manuchoadero@gmail.com>
2023-03-16 12:37:36 +0000

    print data in terminal using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * printf - Print data encoded in quotes in the terminal
+ *
+ * Return: Always return 0 (success)
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
