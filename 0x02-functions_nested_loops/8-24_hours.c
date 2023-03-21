29edfbcb0976d765f2ab3e4f433b5eb3fe179799
manucho254 <manuchoadero@gmail.com>
2023-03-21 21:07:05 +0000

    function to print all minutes of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -23,10 +23,7 @@ void jack_bauer(void)
 				_putchar(':');
 				_putchar((x / 10) + '0');
 				_putchar((x % 10) + '0');
-				if (hour != 23 || x != 59)
-				{
-					_putchar('\n');
-				}
+				_putchar('\n');
 			}
 		}
 	}
