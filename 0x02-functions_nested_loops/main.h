284635af2c746815d4d44af0b990adcc9787346a
manucho254 <manuchoadero@gmail.com>
2023-03-21 13:55:51 +0000

    check if a character is lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,7 @@ void print_alphabet(void);
 /** function to print the alphanbet in lowercase ten times */
 
 void print_alphabet_x10(void);
+
+/** function to check if character is lowercase */
+
+void _is_lower(int c);
