0ced322ba564707735fb81fd9c510dc6e320253d
manucho254 <manuchoadero@gmail.com>
2023-03-22 19:26:44 +0000

    find to print a times table for an integer value

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,67 @@
 #include "main.h"
 
+void print_times_table_values(int val);
+
 /**
- * times_table - function to print the multiple of numbers between 0 and 9
+ * print_times_table - function to print the multiple,
+ * of numbers between 0 and 9
+ *
+ * @n: value to get the times table
  */
 
-void times_table(int n)
+void print_times_table(int n)
+{
+	if (n <= 15 || n < 0)
+	{
+		print_times_table(n);
+	}
+}
+
+/**
+ * print_times_table_values - function to help with the printing of,
+ * the times table values
+ *
+ * @val: value to use to create the times table
+ */
+void print_times_table_values(int val)
 {
 	int x;
 	int y;
 	int times;
 
-	if (n <= 15 || n < 0)
+	for (x = 0; x < val; x++)
 	{
-		for (x = 0; x < n; x++)
-	
+		for (y = 0; y < val; y++)
 		{
-			for (y = 0; y < n; y++)
+			times = x * y;
+			if (y != 0)
 			{
-				times = x * y;
-				if (y != 0)
+				if (times < 10)
 				{
-					if (times < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-				if (times > 9)
-				{
-					_putchar((times / 10) + '0');
-					_putchar((times % 10) + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 				}
 				else
 				{
-					_putchar(times + '0');
-				}
-				if (y != n)
-				{
-					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 			}
-			_putchar('\n');
+			if (times > 9)
+			{
+				_putchar((times / 10) + '0');
+				_putchar((times % 10) + '0');
+			}
+			else
+			{
+				_putchar(times + '0');
+			}
+			if (y != val)
+			{
+				_putchar(',');
+			}
 		}
+		_putchar('\n');
 	}
 }
