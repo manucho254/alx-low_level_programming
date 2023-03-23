0091bf72ea5797e1be1a61d931f21bcc2eaef03a
manucho254 <manuchoadero@gmail.com>
2023-03-23 15:51:36 +0000

    function to solve the FizzBuzzz question

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - function that solve the FizzBuzz question
+ *
+ * print FizzBuzz if number is a multiple of 3 and 5
+ * print Fizz if number is a multiple of 3
+ * print Buzz if number is a multiple of 5
+ * Return: return 0 if success and 1 if error
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	return (0);
+}
