390d274e320b5cb8ed26eaabd8898904036aad8b
manucho254 <manuchoadero@gmail.com>
2023-03-24 15:56:39 +0000

    print numbers using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,100 +13,20 @@ void numbers_greater_than_zero(int n);
 
 void print_number(int n)
 {
-	if (n < 0)
+	double new = 0;
+	double rev_n = 0;
+
+	while (n >= 1)
 	{
-		numbers_less_than_zero(n);
+		new *= 10 + (n % 10);
+		n /= 10;
 	}
-	else
+
+	while (new >= 1)
 	{
-		numbers_greater_than_zero(n);
+		rev_n *= 10 + (n % 10);
+		new /= 10;
 	}
+
+	_putchar("%u", rev_n)
 }
-
-/**
- * numbers_less_than_zero - function to print numbers less than zero
- *
- * @n: value that is less than 0
- */
-
-void numbers_less_than_zero(int n)
-{
-	int new;
-
-	new = n * -1;
-	if (new < 10)
-	{
-		_putchar('-');
-		_putchar(new + '0');
-	}
-	else if (new >= 10 && new <= 99)
-	{
-		_putchar('-');
-		_putchar((new / 10) + '0');
-		_putchar((new % 10) + '0');
-	}
-	else if (new >= 100 && new <= 999)
-	{
-		_putchar('-');
-		_putchar(((new / 10) / 10) + '0');
-		_putchar(((new / 10) % 10) + '0');
-		_putchar(new % 10 + '0');
-	}
-	else if (new >= 1000 && new <= 9999)
-	{
-		_putchar('-');
-		_putchar(((new / 100) / 10) + '0');
-		_putchar(((new / 100) % 10) + '0');
-		_putchar(((new / 10) % 10) + '0');
-		_putchar(new % 10 + '0');
-	}
-	else if (new >= 10000 && new <= 99999)
-	{
-		_putchar('-');
-		_putchar(((new / 1000) / 10) + '0');
-		_putchar(((new / 1000) % 10) + '0');
-		_putchar(((new / 100) % 10) + '0');
-		_putchar(((new / 10) % 10) + '0');
-		_putchar(new % 10 + '0');
-	}
-}
-
-/**
- * numbers_greater_than_zero - function to print values greater than 0
- *
- * @n: value than is greater than 0
- */
-
-void numbers_greater_than_zero(int n)
-{
-	if (n < 10)
-	{
-		_putchar(n + '0');
-	}
-	else if (n >= 10 && n <= 99)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar(((n / 10) / 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 1000 && n <= 9999)
-	{
-		_putchar(((n / 100) / 10) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 10000 && n <= 99999)
-	{
-		_putchar(((n / 1000) / 10) + '0');
-		_putchar(((n / 1000) % 10) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar(n % 10 + '0');
-	}
-}															         }
