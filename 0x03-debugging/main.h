3cb3b1fa89cfccec3724e91724e84eb628ffe810
manucho254 <manuchoadero@gmail.com>
2023-03-24 19:31:25 +0000

    update main.h file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,6 +11,15 @@ void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day)
 
 /**
  * print_remaining_days - function to check for normal and leap years
