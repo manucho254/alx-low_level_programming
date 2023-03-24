81ab0f647610b679d450d15b2099c0899fe7fff6
manucho254 <manuchoadero@gmail.com>
2023-03-24 12:36:11 +0000

    update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - function to find positive or negative number
+ */
+
+void positive_or_negative(int i);
+
 /**
  * function to find the largest between 3 numbers
  * Return: returns the largest integer 
