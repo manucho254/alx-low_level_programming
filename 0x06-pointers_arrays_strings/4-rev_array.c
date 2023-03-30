e0a8148390f750a9dd9b013bc603a946e7447025
manucho254 <manuchoadero@gmail.com>
2023-03-30 15:38:09 +0000

    function to reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,23 +10,15 @@ void reverse_array(int *a, int n)
 {
 	int len = (n - 1); /** number of items in array - 1 */
 	int mid = (n / 2); /** middle of the array */
-	int tmp; /** temporary variable */
 	int x;
 
-	if (n == 2)
+	for (x = 0; x <= mid; x++)
 	{
-		tmp = a[0];
-		a[0] = a[1];
-		a[1] = tmp;
-	}
-	else
-	{
-		for (x = 0; x <= mid; x++)
-		{
-			tmp = a[x];
-			a[x] = a[len];
-			a[len] = tmp;
-			len--;
-		}
+		int tmp;
+
+		tmp = a[x];
+		a[x] = a[len];
+		a[len] = tmp;
+		len--;
 	}
 }
