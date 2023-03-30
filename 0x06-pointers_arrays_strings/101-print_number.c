ae4fdbb35978ac5fbef95a52303ff5e9e2553418
manucho254 <manuchoadero@gmail.com>
2023-03-30 13:26:16 +0000

    function to print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,15 +9,29 @@ void print_number(int n)
 {
 	unsigned int tmp = 0;
 
-	while (n >= 1)
+	if (n == 0)
 	{
-		tmp = (tmp * 10) + (n % 10);
-		n = n / 10;
+		_putchar('0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			_putchar('-');
+		}
+
+		while (n >= 1)
+		{
+			tmp = (tmp * 10) + (n % 10);
+			n = n / 10;
+		}
+		while (tmp >= 1)
+		{
+			_putchar((tmp % 10) + '0');
+			tmp = tmp / 10;
+		}
 	}
 
-	while (tmp >= 1)
-	{
-		_putchar((tmp % 10) + '0');
-		tmp = tmp / 10;
-	}
+	_putchar('\n');
 }
