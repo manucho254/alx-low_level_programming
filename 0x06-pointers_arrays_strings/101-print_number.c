d55760162d66eda94f3aa1976824311f843f25ee
manucho254 <manuchoadero@gmail.com>
2023-03-31 19:42:23 +0000

    function to print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <limits.h>
+#include <math.h>
+
+int convert_negative_to_positive(int n);
+double reverse_number(int n, double tmp, double ret);
 
 /**
  * print_number - function to print a number in terminal
@@ -7,8 +12,8 @@
 
 void print_number(int n)
 {
-	unsigned int ret = 0;
-	unsigned int tmp = n;
+	double ret = 0;
+	double tmp = n;
 
 	if (n == 0)
 	{
@@ -16,27 +21,83 @@ void print_number(int n)
 		return;
 	}
 
+	/** convert negative numbers to positive */
 	if (n < 0)
 	{
-		n = (n * -1);
-		_putchar('-');
+		n = convert_negative_to_positive(n);
 	}
 
-	while (n > 0)
-	{
-		ret = (ret * 10) + (n % 10);
-		n = n / 10;
-	}
+	/** reverse the integer number */
+	ret = reverse_number(n, tmp, ret);
 
-	while (ret > 0)
+	while (ret >= 1)
 	{
-		_putchar((ret % 10) + '0');
+		/** fmod - function to return the remainder of two doubles */
+		_putchar(fmod(ret, 10.0) + '0');
 		ret = ret / 10;
 	}
 
-	while (tmp % 10 == 0)
+	while ((int)tmp % 10 == 0)
 	{
 		_putchar(0 + '0');
 		tmp /= 10;
 	}
 }
+
+/**
+ * reverse_number - function to reverse an integer
+ * @n: integer to be reversed
+ * @ret: double variable to hold the reversed integer value
+ * @tmp: temprary variable to hold the value of n as a double
+ * Return: double representing the reversed integer
+ */
+
+double reverse_number(int n, double tmp, double ret)
+{
+	int x = 0;
+
+	while (n >= 1)
+	{
+		if (tmp == INT_MIN && x == 0)
+		{
+			/**
+			 * we add one because we did a deduction in INT_MIN,
+			 * in the convert_negative_to_positive function
+			 */
+			ret = (ret * 10) + (n % 10) + 1;
+		}
+		else
+		{
+			ret = (ret * 10) + (n % 10);
+		}
+		n = n / 10;
+		x++;
+	}
+
+	return (ret);
+}
+
+/**
+ * convert_negative_to_positive - function to convert,
+ * a negative number to positive
+ * @n: number to be converted
+ * Return: @n as positive number
+ */
+
+int convert_negative_to_positive(int n)
+{
+	if (n == INT_MIN)
+	{
+		/** we minus one so that we can store n as an in INT_MAX */
+		n = (n + -1);
+	}
+	else
+	{
+		n = (n * -1);
+		_putchar('-');
+	}
+
+	return (n);
+}
+
+
