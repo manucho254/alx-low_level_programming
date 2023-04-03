635ebdaf3919665fd9c0c2f3e9933173fc920b7d
manucho254 <manuchoadero@gmail.com>
2023-04-03 19:34:50 +0000

    function that searches a string for any a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,7 +16,7 @@ char *_strpbrk(char *s, char *accept)
 	int x;
 	int y;
 
-	for (x = 0; s[x] != '\0' && s[x] != ' '; x++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
 		for (y = 0; accept[y] != '\0'; y++)
 		{
