6b805d47041d14d3a68a994b0646a4ee313b46a9
manucho254 <manuchoadero@gmail.com>
2023-04-04 17:57:21 +0000

    function to print sum of all even fibonnacci numbers less than 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,35 +10,21 @@ int main(void)
 	int x;
 	long int n_1 = 0, n_2 = 1;
 	long int next = n_1 + n_2;
+	long int sum = 0;
 
 	for (x = 2; next <= 4000000; x++)
 	{
-		if (x <= 2)
+		n_1 = n_2;
+		n_2 = next;
+	    next = n_1 + n_2;
+
+		if (next % 2 == 0)
 		{
-			if (x == 2)
-			{
-				printf("%d,", 1);
-			}
-			else
-			{
-				printf(" %d,", 1);
-			}
-		}
-		else
-		{
-			n_1 = n_2;
-			n_2 = next;
-			next = n_1 + n_2;
-			if (next >= 4000000)
-			{
-				printf(" %ld", next);
-			}
-			else
-			{
-				printf(" %ld,", next);
-			}
+			sum += next;
 		}
 	}
+
+	printf("%ld", sum);
 	printf("\n");
 
 	return (0);
