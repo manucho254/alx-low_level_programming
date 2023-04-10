1c0135aecaf94dfe94c77f6c17f6e317ca6602dd
manucho254 <manuchoadero@gmail.com>
2023-04-10 21:35:35 +0000

    creating static libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,7 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c)
 {
-	(void) c;
-	return (0);
+	char ch = (char) c; /* convert the integer to a character */
+
+
+	return write(STDOUT_FILENO, &ch, 1); /** 
+										  * Write character to,
+										  * the standard output
+										  */
 }
