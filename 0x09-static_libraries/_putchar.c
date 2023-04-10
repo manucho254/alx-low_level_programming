e4fb5c136f66684445038f137dc2e37232a71dc8
manucho254 <manuchoadero@gmail.com>
2023-04-10 21:45:54 +0000

    creating static libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * _putchar - function to print a single character in terminal
+ * @c: character to be printend in terminal
+ * Return: an integer of the value to be printed in ther terminal
+ */
+
 int _putchar(char c)
 {
 	char ch = (char) c; /* convert the integer to a character */
 
 
-	return write(STDOUT_FILENO, &ch, 1); /** 
-										  * Write character to,
-										  * the standard output
-										  */
+	return (write(STDOUT_FILENO, &ch, 1));/**
+										   * Write character to,
+										   * the standard output
+										   */
 }
