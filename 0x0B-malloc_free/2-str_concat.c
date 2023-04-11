cc3ade0c1707a50db43d25a19a7af83c06d1330c
manucho254 <manuchoadero@gmail.com>
2023-04-11 17:41:43 +0000

    function to concatnate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
+int _strlen(char *s);
 /**
  * *str_concat - function to concatnate two strings
  * @s1: string number one
@@ -10,17 +10,18 @@
  * else return NULL
  */
 
-
 char *str_concat(char *s1, char *s2)
 {
-	int size_s1 = strlen(s1); /** variable to hold the size of pointer s1 */
-	int size_s2 = strlen(s2); /** variable to hold the size of pointer s2 */
+	int size_s1 = _strlen(s1); /** variable to hold the size of pointer s1 */
+	int size_s2 = _strlen(s2); /** variable to hold the size of pointer s2 */
 	int size_s3 = 0; /** variable to hold the size of  pointer s3 */
 	int x, y;
 	char *s3; /** pointer to new memory location */
 
-	size_s3 = (size_s1 + size_s2 + 1); /** update the size of s3 */
-	s3 = malloc(sizeof(char) * (size_s3)); /** allocate memory of size_s3 to s3*/
+	size_s3 = (size_s1 + size_s2); /** update the size of s3 */
+
+	/** allocate memory of size_s3 to s3*/
+	s3 = malloc(sizeof(char) * (size_s3 + 1));
 
 	if (!s3)
 	{
@@ -48,4 +49,26 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	return (s3);
+}
+
+/**
+ * _strlen - function to get length of a string
+ * @s: string to check the length
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int j;
+	int size = 0;
+
+	if (s)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			size += 1;
+		}
+	}
+
+	return (size);
 }
