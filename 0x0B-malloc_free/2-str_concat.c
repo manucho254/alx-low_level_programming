8c90921b7c93d69f75694f48bf8b2c971b5d140d
manucho254 <manuchoadero@gmail.com>
2023-04-11 17:26:50 +0000

    function to concatnate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *str_concat - function to concatnate two strings
+ * @s1: string number one
+ * @s2: string number two
+ * Return: pointer to the newly allocated space in memory,
+ * else return NULL
+ */
+
+
+char *str_concat(char *s1, char *s2)
+{
+	int size_s1 = strlen(s1); /** variable to hold the size of pointer s1 */
+	int size_s2 = strlen(s2); /** variable to hold the size of pointer s2 */
+	int size_s3 = 0; /** variable to hold the size of  pointer s3 */
+	int x, y;
+	char *s3; /** pointer to new memory location */
+
+	size_s3 = (size_s1 + size_s2 + 1); /** update the size of s3 */
+	s3 = malloc(sizeof(char) * (size_s3)); /** allocate memory of size_s3 to s3*/
+
+	if (!s3)
+	{
+		return (NULL);
+	}
+
+	s3[size_s3] = '\0'; /** add a null pointer to the end of the array */
+
+	if (s1)
+	{
+		/** add data from s1 to s3 */
+		for (x = 0; s1[x] != '\0'; x++)
+		{
+			s3[x] = s1[x];
+		}
+	}
+	if (s2)
+	{
+		/** add data from s2 to s3 */
+		for (y = 0; s2[y] != '\0'; y++)
+		{
+			s3[x] = s2[y];
+			x++;
+		}
+	}
+
+	return (s3);
+}
