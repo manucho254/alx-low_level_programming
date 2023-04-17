966e4b653459eebbc1c28e642e34e32a34bf9770
manucho254 <manuchoadero@gmail.com>
2023-04-17 21:21:12 +0000

    function to initialize dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <string.h>
 
 /**
  * init_dog - function to initialize variable of type dog
@@ -10,7 +11,17 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	char nil[] = "nil";
+
 	/** method two on initializing a variable of type dog */
+	if (name == NULL)
+	{
+		name = nil;
+	}
+	if (owner == NULL)
+	{
+		owner = nil;
+	}
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
