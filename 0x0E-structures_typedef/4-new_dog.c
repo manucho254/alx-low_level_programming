15442857f058ce7e57a365285884b41ec8076f44
manucho254 <manuchoadero@gmail.com>
2023-04-17 20:20:55 +0000

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,24 +1,10 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
-
-char *copy_string(char *str, int len);
-/**
- * dog_t *new_dog - function that creates a new dog
- * @name: pointer to dogs name
- * @age: age of the dog
- * @owner: pointer to the owner of the dog
- * Return: pointer refrencing varible of type dog_t
- */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	/** creating a new variable of type dog_t*/
 	dog_t *nice_dog;
-	/**
-	 *int len_name = strlen(name);
-	 *int len_owner = strlen(owner);
-	 */
 
 	nice_dog = malloc(sizeof(dog_t));
 
@@ -35,33 +21,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (nice_dog);
 }
-
-/**
- * *copy_string - function to copy string
- * @str: string to be copied
- * @len: size of the string
- * Return: a pointer to string array
- */
-
-char *copy_string(char *str, int len)
-{
-	char *s;
-	int x;
-
-	s = malloc(sizeof(char) * len);
-
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-
-	s[len] = '\0';
-
-	for (x = 0; x < len; x++)
-	{
-		s[x] = str[x];
-	}
-
-	return (s);
-}
-
