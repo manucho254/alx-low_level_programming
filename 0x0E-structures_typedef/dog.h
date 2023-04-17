1bbfb93607a2b263d7147cb748381eb06c7fa628
manucho254 <manuchoadero@gmail.com>
2023-04-17 18:40:37 +0000

    created new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define dog_h__
 
 /**
- * struct dog - structure for a dog
+ * struct dog - type dog
  * @name: name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
@@ -14,6 +14,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
