bd5945083c5f1eff967b31473b3fa7bdb186588b
manucho254 <manuchoadero@gmail.com>
2023-04-20 19:11:18 +0000

    function that prints everything it gets in the arguments in the termiinal

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_all("ceisf", 'B', 3, "stSchool", 2.3);
+	return (0);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - function that prints anything
+ * @format: is a list of types of arguments passe in function
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int x;
+	va_list args;
+
+	va_start(args, format);
+
+	x = 0;
+	while (format[x] != '\0')
+	{
+		if (format[x] == 'c')
+		{
+			/** chars are converted to int */
+			printf("%c", va_arg(args, int));
+		}
+		else if (format[x] == 'i')
+		{
+			printf("%d", va_arg(args, int));
+		}
+		else if (format[x] == 'f')
+		{
+			printf("%f", va_arg(args, double));
+		}
+		else if (format[x] == 's')
+		{
+			printf("%s", va_arg(args, char *));
+		}
+		if (x != '\0' && format[x + 1] != '\0')
+		{
+			printf(", ");
+		}
+		x++;
+	}
+	va_end(args);
+	printf("\n");
+}
