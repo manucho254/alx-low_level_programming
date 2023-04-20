0f5d344975ade063183f813b27d1c8782a0a5c52
manucho254 <manuchoadero@gmail.com>
2023-04-20 17:17:53 +0000

    function that print all the numbers in the from arguments in ther terminal

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function to print number we get as arguments int terminal
+ * @separator: pointer to separator to use while printing numbers
+ * @n: number of arguments we get in the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int x;
+
+	va_start(args, n); /** initialize argument list*/
+	for (x = 0; x < n; x++)
+	{
+		/** print values */
+		if (separator == NULL)
+		{
+			if (x != (n - 1))
+			{
+				printf("%d ", va_arg(args, unsigned int));
+			}
+			else
+			{
+				printf("%d", va_arg(args, unsigned int));
+			}
+		}
+		if (separator != NULL && x < (n - 1))
+		{
+			printf("%d%s", va_arg(args, unsigned int), separator);
+		}
+		else
+		{
+			printf("%d", va_arg(args, unsigned int));
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
