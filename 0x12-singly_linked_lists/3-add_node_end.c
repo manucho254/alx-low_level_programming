294b2266bf4046a1d890531b0f87fb141f3b8e99
manucho254 <manuchoadero@gmail.com>
2023-04-27 13:27:59 +0000

    function to add node at the end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,6 +14,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *last;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
 	/** allocate memory for pointer new of type list_t */
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
