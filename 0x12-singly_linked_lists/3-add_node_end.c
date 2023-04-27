bba741d1bcbcac74af9f66d5e01eae2e0d937158
manucho254 <manuchoadero@gmail.com>
2023-04-27 13:24:48 +0000

    function to add node at the end of alinked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,15 +4,15 @@
 #include <stdlib.h>
 
 /**
- * add_node - function to add node to linked list
+ * add_node_end - function to add node to linked list
  * @head: structure of type list_t
  * @str: string for value of linked list
  * Return: number of nodes
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new, *last;
 
 	/** allocate memory for pointer new of type list_t */
 	new = malloc(sizeof(list_t));
@@ -20,20 +20,26 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	/** setting the last value */
+	last = (*head);
 
 	/** add value to new->str */
 	new->str = strdup(str);
 	new->len = strlen(new->str);
+	new->next = NULL;
 
 	if ((*head) == NULL)
 	{
 		(*head) = new;
 		return (NULL);
 	}
-	/** make new->next point to the pointer of *head */
-	new->next = (*head);
-	new->next = (*head);
-	(*head) = new;
 
-	return (*head);
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	last->next = new;
+
+	return (last);
 }
