afd99b41dc0a13b69c1c257944b835cd0a3e813d
manucho254 <manuchoadero@gmail.com>
2023-04-27 16:53:33 +0000

    print string using assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,27 @@
+   SECTION .data
+message: db  "Hello, Holberton", 0
+fmt: db "%s", 10, 0
+new_line: db "\n", 0
+fmt2: db "%c", 1, 0
+
+   SECTION .text
+   extern printf
+   global main
+
+main:
+_start:
+  mov esi, message ;64-bit Abi passing order
+  mov edi, fmt
+  mov eax, 0   ; printf is varargs so eax counts
+  ; call printf
+  call printf
+
+  mov esi, new_line ;64-bit Abi passing order
+  mov edi, fmt2
+  mov eax, 0   ; printf is varargs so eax counts
+  ; call printf
+  call printf
+
+  mov ebx, 0 ;normal-exit code
+  mov eax, 1 ;process termination service
+  int 0x80  ;linux kernel service
---
+++ 0x12-singly_linked_lists/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 if success
+ */
+
+int main(void)
+{
+	printf("Hello, Holberton\n");
+	return (0);
+}
