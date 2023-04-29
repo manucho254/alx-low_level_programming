566c1219d481122f1d8a5486d7d0db2c3c309624
manucho254 <manuchoadero@gmail.com>
2023-04-29 11:11:06 +0000

    program to free memory for a linked list and sets the list to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function to free memory of head,
+ * and sets head to NULL
+ * @head: linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	while ((*head))
+	{
+		listint_t *tmp;
+
+		tmp = (*head);
+		(*head) = (*head)->next;
+		free(tmp);
+	}
+	(*head) = NULL;
+}
+
