fdc67eb72d05438dc1067a3231c2edfa2a4a1984
manucho254 <manuchoadero@gmail.com>
2023-05-02 19:47:27 +0000

    program to add node at specified index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new;
 		return (*head);  /** added new node to head */
 	}
-	if ((*head) == NULL && idx == 0)
+	if ((*head) == NULL || idx == 0)
 	{
 		free(new);
 		return (NULL);
