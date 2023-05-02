79a3915b9f54049714676fbb94b6c7b3f50475e8
manucho254 <manuchoadero@gmail.com>
2023-05-02 19:26:01 +0000

    function that free memory and sets a pointer to pointer to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,9 @@
 
 void free_listint2(listint_t **head)
 {
-	if ((*head) == NULL)
-	{
-		head = NULL;
-		return;
-	}
+	listint_t *new;
+
+	new = NULL;
 	while ((*head))
 	{
 		listint_t *tmp;
@@ -23,6 +21,6 @@ void free_listint2(listint_t **head)
 		(*head) = (*head)->next;
 		free(tmp);
 	}
-	head = NULL;
+	head = &new;
 }
 
