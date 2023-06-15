ca73eb7937a8cc3310a15d683fc89932c60ff762
manucho254 <manuchoadero@gmail.com>
2023-06-15 17:16:47 +0000

    function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	/** check if the index was found */
-	if (!found)
+	if (found == 0)
 		return (NULL);
 
 	/** update value of (*h) */
