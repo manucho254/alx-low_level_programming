2b5d96cfbd05791aedf94116a218ab3883d1b34a
manucho254 <manuchoadero@gmail.com>
2023-06-15 17:14:40 +0000

    function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -25,6 +25,8 @@ int main(void)
 	print_dlistint(head);
 	printf("-----------------\n");
 	insert_dnodeint_at_index(&head, 5, 4096);
+	insert_dnodeint_at_index(&head, 2, 40);
+	insert_dnodeint_at_index(&head, 7, 100);
 	print_dlistint(head);
 	free_dlistint(head);
 	head = NULL;
