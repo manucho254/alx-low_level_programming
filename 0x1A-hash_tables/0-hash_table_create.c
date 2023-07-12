149f3415920eb283c466501ac01004e4c6b0835c
manucho254 <manuchoadero@gmail.com>
2023-07-12 17:53:38 +0000

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,15 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int x;
 
+	/** allocate memory for hash table */
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
+	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
 
-	for (x = 0; x < size; x++)
+	for (x = 0; x < table->size; x++)
 		table->array[x] = NULL;
 
 	return (table);
