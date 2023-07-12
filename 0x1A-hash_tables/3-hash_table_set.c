0ef434036101e385b7205e6959b7c4dfd3944358
manucho254 <manuchoadero@gmail.com>
2023-07-12 21:32:11 +0000

    add item to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, x;
 	hash_node_t *item;
 
-	if (ht == NULL || strcmp(key, "") == 0 || key == NULL)
+	if (ht == NULL || strcmp(key, "") == 0 || key == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
