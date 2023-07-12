f5823f65599b403c1d018de1d4ec363ebb9940f9
manucho254 <manuchoadero@gmail.com>
2023-07-12 22:15:31 +0000

    get value in hash table using a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index, x;
 	char *value;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
