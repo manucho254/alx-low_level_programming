73d87acafdb974a2e4a476355f72ce7934583761
manucho254 <manuchoadero@gmail.com>
2023-07-12 21:08:14 +0000

    add item to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,102 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+hash_node_t *add_node_end(hash_node_t *h, const char *key, const char *value);
+
+/**
+ * hash_table_set - add new element to hash table
+ *
+ * @ht: pointer to hash table
+ * @key: key for hash node
+ * @value: value for a hash node.
+ *
+ * Return: 1 if success else return 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index, x;
+	hash_node_t *item, *head;
+
+	if (ht == NULL || strcmp(key, "") == 0 || key == NULL || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	item = malloc(sizeof(hash_node_t));
+	/** check for malloc error */
+	if (item == NULL)
+		return (0);
+
+	item->key = (char *)key;
+	item->value = strdup(value);
+	item->next = NULL;
+
+	/** iterate through the hash table find the index and add an item */
+	for (x = 0; x < ht->size; x++)
+	{
+		if (x == index && ht->array[x] == NULL)
+		{
+			ht->array[x] = item;
+			break;
+		}
+		if (x == index && ht->array[x] != NULL)
+		{
+			head = malloc(sizeof(hash_node_t));
+			if (head == NULL)
+				return (0);
+			head = ht->array[x];
+			add_node_end(head, key, value);
+			break;
+		}
+	}
+
+	return (1);
+}
+
+
+/**
+ * add_node_end - function to add node to hash_node_t
+ *
+ * @h: structure of type hash_node_t
+ * @key: key for hash node
+ * @value: value for a hash node.
+ *
+ * Return: pointer to new node
+ */
+
+hash_node_t *add_node_end(hash_node_t *h, const char *key, const char *value)
+{
+	hash_node_t *new, *last;
+
+	/** allocate memory for pointer new of type hash_node_t */
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	/** setting the last value */
+	last = (h);
+
+	if (key != NULL)
+	{
+		/** add value to new->key */
+		new->key = (char *)key;
+		new->value = strdup(value);
+		new->next = NULL;
+	}
+	if (h == NULL)
+	{
+		h = new;
+		return (h);
+	}
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	last->next = new;
+
+	return (new);
+}
---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+int main(void)
+{
+	hash_table_t *ht;
+	unsigned long int x;
+
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "betty", "cool");
+	hash_table_set(ht, "hetairas", "fun");
+	hash_table_set(ht, "mentioner", "yes");
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x])
+		{
+			hash_node_t *head = ht->array[x];
+
+			while (head)
+			{
+				printf("%s | %s\n", head->key, head->value);
+				head = head->next;
+			}
+		}
+	}
+	
+	return (EXIT_SUCCESS);
+}
