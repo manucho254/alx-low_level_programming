9390d5054154ccc32c6bd22d68e27cbb012d429d
manucho254 <manuchoadero@gmail.com>
2023-08-07 14:52:53 +0000

    fixes to fib function

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,17 @@
 int main(void)
 {
 	int x;
-	float n_1 = 0, n_2 = 1;
-	float next = n_1 + n_2;
+	unsigned long int n_1 = 0, n_2 = 1;
+	unsigned long int next = n_1 + n_2;
 
 	for (x = 2; x <= 99; x++)
 	{
 		if (x <= 2)
 		{
 			if (x == 2)
-			{
 				printf("%d,", 1);
-			}
 			else
-			{
 				printf(" %d,", 1);
-			}
 		}
 		else
 		{
@@ -31,13 +27,9 @@ int main(void)
 			n_2 = next;
 			next = n_1 + n_2;
 			if (x == 99)
-			{
-				printf(" %.0f", next * 1000);
-			}
+				printf(" %ld", next);
 			else
-			{
-				printf(" %.0f,", next * 1000);
-			}
+				printf(" %ld,", next);
 		}
 	}
 	printf("\n");
