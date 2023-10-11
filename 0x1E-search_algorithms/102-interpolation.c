fb5295b53eaa8cf55bd0f996d1dea73b63852499
manucho254 <manuchodero@gmail.com>
2023-10-11 17:29:46 +0000

    function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -25,7 +25,10 @@ int interpolation_search(int *array, size_t size, int value)
 		pos = (low + (((double)(high - low)
 		       / (array[high] - array[low])) * (value - array[low])));
 
-		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (pos < size)
+			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		else
+			printf("Value checked array[%ld] is out of range\n", pos);
 
 		if (array[pos] < value)
 			low = pos + 1;
@@ -38,6 +41,5 @@ int interpolation_search(int *array, size_t size, int value)
 	if (value == array[low])
 		return (low);
 
-	printf("Value checked array[%ld] is out of range\n", low);
 	return (-1);
 }
